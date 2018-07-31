f9839bac16220be1c36d119630da707b8cec5afe
Donald Coffin <donald.coffin@gmail.com>
2018-07-31 17:58:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,28 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+return array
+}
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+
+}
+function accessElementInArray(array, index) {
+  return (array[index]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  days.shift(array)
+  return array
+}
